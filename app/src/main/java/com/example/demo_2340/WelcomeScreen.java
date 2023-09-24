d82bf280c8a3bab300f915f691bf8689999c87cc
Jin Lee <jinlee@Jins-MBP-M2.attlocal.net>
2023-09-24 17:27:16 -0400

    added welcome screen

---
+++ app/src/main/java/com/example/demo_2340/WelcomeScreen.java
@@ -0,0 +1,35 @@
+package com.example.demo_2340;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button startGameButton = findViewById(R.id.start_button);
+        Button exitGameButton = findViewById(R.id.exit_button);
+
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(WelcomeScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        exitGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
