c59c7195f2423f01cc2da4100c55f6d625cbe311
jarizono <jarizono6@gatech.edu>
2023-09-28 11:20:31 -0400

    fixed end screen

--- app/src/main/java/com/example/demo_2340/EndScreenActivity.java
+++ app/src/main/java/com/example/demo_2340/EndScreenActivity.java
@@ -1,16 +1,12 @@
 package com.example.demo_2340;
 
-<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity;
-=======
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
->>>>>>> julia
+
 
 public class EndScreenActivity extends AppCompatActivity {
     private TextView scoreTextView;
@@ -29,11 +25,7 @@ public class EndScreenActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // restart the game activity
-<<<<<<< HEAD
-                Intent intent = new Intent(EndScreenActivity.this, GameActivity.class);
-=======
                 Intent intent = new Intent(EndScreenActivity.this, ConfigActivity.class);
->>>>>>> julia
                 startActivity(intent);
             }
         });
@@ -42,16 +34,9 @@ public class EndScreenActivity extends AppCompatActivity {
 
 
     // to start ending screen activity from game activity
-<<<<<<< HEAD
-    Intent intent = new Intent(GameActivity.this, EndScreenActivity.class);
-    intent.putExtra("score", playerScore);
-    startActivity(intent);
-=======
     /**
     Intent intent = new Intent(GameScreen.this, EndScreenActivity.class);
     intent.putExtra("score", playerScore);
     startActivity(intent);
      */
-
->>>>>>> julia
 }
