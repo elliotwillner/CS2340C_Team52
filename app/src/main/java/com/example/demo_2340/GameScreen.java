8203cb4a29b7d87a1e9793cd027df8cbd7c99ad5
jarizono <jarizono6@gatech.edu>
2023-10-09 18:54:01 -0400

    updated game screen to display score

--- app/src/main/java/com/example/demo_2340/GameScreen.java
+++ app/src/main/java/com/example/demo_2340/GameScreen.java
@@ -9,20 +9,40 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Button;
+import android.os.Handler;
 
 /*
  * Main Activity class that loads {@link MainFragment}.
  */
 public class GameScreen extends AppCompatActivity {
+    private TextView nameTextView;
+    private TextView healthTextView;
+    private ImageView spriteImageView;
+    private TextView difficultyTextView;
+    private TextView playerScoreTextView;
+    private static int playerScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        playerScoreTextView = findViewById(R.id.playerScoreTextView);
+        playerScore = 101;
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                playerScore--;
+                playerScoreTextView.setText("Score: " + String.valueOf(playerScore));
+                handler.postDelayed(this, 1000);
+            }
+        };
 
-        TextView nameTextView = findViewById(R.id.nameTextView);
-        TextView healthTextView = findViewById(R.id.healthTextView);
-        ImageView spriteImageView = findViewById(R.id.spriteImageView);
-        TextView difficultyTextView = findViewById(R.id.difficultyTextView);
+        handler.postDelayed(runnable, 1000);
+
+        nameTextView = findViewById(R.id.nameTextView);
+        healthTextView = findViewById(R.id.healthTextView);
+        spriteImageView = findViewById(R.id.spriteImageView);
+        difficultyTextView = findViewById(R.id.difficultyTextView);
 
         String playerName = getIntent().getStringExtra("playerName");
         String spriteID = getIntent().getStringExtra("selectedCharacter");
@@ -58,9 +78,17 @@ public class GameScreen extends AppCompatActivity {
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Player.getInstance().setName(playerName);
+                Player.getInstance().setScore(playerScore);
+                Leaderboard.addPlayer();
+                Player.getInstance().reset();
+                handler.removeCallbacks(runnable);
                 Intent intent = new Intent(GameScreen.this, EndScreenActivity.class);
                 startActivity(intent);
             }
         });
+    }
+    public int getScore() {
+        return playerScore;
     }
 }
