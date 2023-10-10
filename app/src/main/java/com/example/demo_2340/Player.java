6612502ec4ad9684bf7b38ece72e48ac6ee01b19
jarizono <jarizono6@gatech.edu>
2023-10-10 15:45:43 -0400

    added date

--- app/src/main/java/com/example/demo_2340/Player.java
+++ app/src/main/java/com/example/demo_2340/Player.java
@@ -1,14 +1,16 @@
 package com.example.demo_2340;
 
-import java.util.ArrayList;
+import java.util.Date;
 
 public class Player {
     private String name;
     private int score;
+    private Date date;
     private volatile static Player uniqueInstance;
     private Player() {
         this.name = "";
         this.score = 0;
+        this.date = null;
     }
     public static Player getInstance() {
         if (uniqueInstance == null) {
@@ -33,11 +35,18 @@ public class Player {
         return score;
     }
 
+    public String getDate() {
+        return date.toString();
+    }
+
     public synchronized void setName(String name) {
         this.name = name;
     }
 
     public synchronized void setScore(int score) {
         this.score = score;
+    }
+    public synchronized void setDate(Date date) {
+        this.date = date;
     }
 }
